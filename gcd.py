c667dfbed51e4f9d138035c6cc3fc1aeab85a22e
Dmitry Osinkin <dm.osinkin@gmail.com>
2017-09-14 15:44:46 +0300

    some changes

--- gcd.py
+++ gcd.py
@@ -20,7 +20,7 @@ def run():
     while True:
         print("Enter a and b:\n")
         try:
-            print(gogogo(int(input()), int(input())))
+            print(gсв(int(input()), int(input())))
         except ValueError:
             print("please, enter only numbers =(")
 
