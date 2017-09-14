5aef3557e283e5f580b422378aea8ff182c3676f
Дмитрий Осинкин <dao@prom-te.ru>
2017-09-14 15:33:17 +0300

    init commit

---
+++ gcd.py
@@ -0,0 +1,27 @@
+"""
+Find the greatest common divisor with my
+uglycode (aka govnocode for russians), muhaha.
+
+Just run this cod in shell!
+
+Author: Osinkin Dmitry, dm.osinkin@gmail.com
+"""
+
+def gcd(a,b):
+    answer = 1
+    maxit = min(a,b)
+    for i in range(1,maxit+1):
+        if a%i is 0 and b%i is 0:
+            print("... ", i)
+            answer=i
+    return answer
+
+def run():
+    while True:
+        print("Enter a and b:\n")
+        try:
+            print(gogogo(int(input()), int(input())))
+        except ValueError:
+            print("please, enter only numbers =(")
+
+run()
